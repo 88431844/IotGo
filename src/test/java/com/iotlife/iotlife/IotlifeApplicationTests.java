a12c5a31640028f1bffd087960b83702fa22ee69
88431844 <88431844@163.com>
2017-09-10 15:57:31 +0800

    第一次提交
    Signed-off-by: miracle <88431844@163.com>

---
+++ src/test/java/com/iotlife/iotlife/IotlifeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iotlife.iotlife;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IotlifeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
