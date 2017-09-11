3d23446b7d75200e83a01ea4986055c0968ab53f
miracle <88431844@163.com>
2017-09-11 23:35:21 +0800

    编写了管理员和用户登录的controller实现
    Signed-off-by: miracle <88431844@163.com>

--- src/main/java/com/iotlife/controller/LoginController.java
+++ src/main/java/com/iotlife/controller/LoginController.java
@@ -3,8 +3,11 @@ package com.iotlife.controller;
 import com.iotlife.dto.AdminDto;
 import com.iotlife.dto.CommonDto;
 import com.iotlife.dto.UserDto;
+import com.iotlife.service.LoginService;
+import com.iotlife.util.myconst;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,22 +15,58 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/login")
 public class LoginController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    private LoginService loginService;
+
+    /**
+     * 管理员登录
+     *
+     * @param adminDto
+     * @return
+     */
     @RequestMapping("/adminLogin")
     @ResponseBody
     public CommonDto adminLogin(@RequestBody @Validated AdminDto adminDto) {
-        CommonDto dto = new CommonDto();
-        return dto;
+        CommonDto commonDto = new CommonDto();
+        AdminDto data = loginService.adminLogin(adminDto);
+
+        if (null != data && null != data.getId()) {
+            //如果查询出来结果不等于null，并且id不为null，则说明登录成功
+            commonDto.setCode(myconst.SUCCESS);
+            commonDto.setMsg(myconst.LOGIN_SUCCESS);
+            commonDto.setData(data);
+        } else {
+            commonDto.setCode(myconst.FAIL);
+            commonDto.setMsg(myconst.LOGIN_FAIL);
+        }
+        return commonDto;
     }
 
+    /**
+     * 用户登录
+     * @param userDto
+     * @return
+     */
     @RequestMapping("/userLogin")
     @ResponseBody
     public CommonDto userLogin(@RequestBody @Validated UserDto userDto) {
-        CommonDto dto = new CommonDto();
-        return dto;
+        CommonDto commonDto = new CommonDto();
+        UserDto data = loginService.userLogin(userDto);
+
+        if (null != data && null != data.getId()) {
+            //如果查询出来结果不等于null，并且id不为null，则说明登录成功
+            commonDto.setCode(myconst.SUCCESS);
+            commonDto.setMsg(myconst.LOGIN_SUCCESS);
+            commonDto.setData(data);
+        } else {
+            commonDto.setCode(myconst.FAIL);
+            commonDto.setMsg(myconst.LOGIN_FAIL);
+        }
+        return commonDto;
     }
+
 }
