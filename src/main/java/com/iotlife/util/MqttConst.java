74e190275e9bdc5b95ba1be5d2631f43e725652d
gaozhihua.aerozhonghuan.com <vv11uhtfsMXT>
2017-09-11 11:12:31 +0800

    修改了MQTT相关常量方式改为写死 不用从配置文件中读取

--- src/main/java/com/iotlife/util/MqttConst.java
+++ src/main/java/com/iotlife/util/MqttConst.java
@@ -5,25 +5,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MqttConst {
-    @Value("${broker.ssl.port:11883}")
-    public static String SSL_PORT = "1883";
 
-    @Value("${broker.jks.path:c://}")
-    public static String JKS_PATH = "c://";
+    public static final String SSL_PORT = "1883";
 
-    @Value("${broker.key.store.password:password}")
-    public static String KEY_STORE_PASSWORD = "password";
+    public static final String JKS_PATH = "c://";
 
-    @Value("${broker.key.manager.password:password}")
-    public static String KEY_MANAGER_PASSWORD = "password";
+    public static final String KEY_STORE_PASSWORD = "password";
 
-    @Value("${broker.secure.websocket.port:8080}")
-    public static String SECURE_WEBSOCKET_PORT = "8080";
+    public static final String KEY_MANAGER_PASSWORD = "password";
 
-    @Value("${broker.port:1883}")
-    public static String PORT = "1883";
+    public static final String SECURE_WEBSOCKET_PORT = "8080";
 
-    @Value("${broker.web.socket.port:18080}")
-    public static String WEB_SOCKET_PORT = "18080";
+    public static final String PORT = "1883";
+
+    public static final String WEB_SOCKET_PORT = "18080";
 
 }
