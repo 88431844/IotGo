d183a0c65004d92ac1aacccd10fbec111e435763
miracle <88431844@163.com>
2017-09-11 23:42:39 +0800

    code码类型变为String
    Signed-off-by: miracle <88431844@163.com>

--- src/main/java/com/iotlife/util/myconst.java
+++ src/main/java/com/iotlife/util/myconst.java
@@ -1,7 +1,9 @@
 package com.iotlife.util;
 
 public class myconst {
-    public static final int SUCCESS = 1111;
-    public static final int FAIL = 9999;
-    public static final int EMPTY_LIST = 1000;
+    public static final String SUCCESS = "1111";
+    public static final String FAIL = "9999";
+    public static final String EMPTY_LIST = "1000";
+    public static final String LOGIN_FAIL = "登录失败";
+    public static final String LOGIN_SUCCESS = "登录成功";
 }
