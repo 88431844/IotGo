9b31a1345f519a29d459acb496be20729404f9cb
miracle <88431844@163.com>
2017-09-15 00:49:37 +0800

    添加结果为null的code码
    Signed-off-by: miracle <88431844@163.com>

--- src/main/java/com/iotlife/util/myconst.java
+++ src/main/java/com/iotlife/util/myconst.java
@@ -6,4 +6,5 @@ public class myconst {
     public static final String EMPTY_LIST = "1000";
     public static final String LOGIN_FAIL = "登录失败";
     public static final String LOGIN_SUCCESS = "登录成功";
+    public static final String NULL_RESULT = "9990";
 }
