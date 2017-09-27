8a072b60f86b71d085cf327fdc09cc4b8df97299
miracle <88431844@163.com>
2017-09-28 00:28:49 +0800

    完成了用户controller层 部分 开发
    Signed-off-by: miracle <88431844@163.com>

--- src/main/java/com/iotlife/controller/UserController.java
+++ src/main/java/com/iotlife/controller/UserController.java
@@ -2,8 +2,11 @@ package com.iotlife.controller;
 
 import com.iotlife.dto.CommonResponseDto;
 import com.iotlife.dto.UserDto;
+import com.iotlife.service.UserService;
+import com.iotlife.util.myconst;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,38 +17,111 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ *用户controller
  */
 @RestController
 public class UserController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private UserService userService;
 
+    /**
+     * 添加用户
+     *
+     * @param userDto
+     * @return
+     */
     @RequestMapping("/addUser")
     @ResponseBody
     public CommonResponseDto addUser(@RequestBody @Validated UserDto userDto) {
-        CommonResponseDto dto = new CommonResponseDto();
-        return dto;
+        CommonResponseDto ret = new CommonResponseDto();
+        try {
+            userService.addUser(userDto);
+            ret.setCode(myconst.SUCCESS);
+        } catch (Exception e) {
+            logger.error("UserController addUser error");
+            ret.setCode(myconst.EXCEPTION);
+            e.printStackTrace();
+        }
+        return ret;
     }
 
+    /**
+     * 删除用户
+     * @param userDto
+     * @return
+     */
     @RequestMapping("/delUser")
     @ResponseBody
     public CommonResponseDto delUser(@RequestBody @Validated UserDto userDto) {
-        CommonResponseDto dto = new CommonResponseDto();
-        return dto;
+        CommonResponseDto ret = new CommonResponseDto();
+        try {
+            userService.delUser(userDto);
+            ret.setCode(myconst.SUCCESS);
+        } catch (Exception e) {
+            logger.error("UserController delUser error");
+            ret.setCode(myconst.EXCEPTION);
+            e.printStackTrace();
+        }
+        return ret;
     }
 
+    /**
+     * 修改用户
+     * @param userDto
+     * @return
+     */
     @RequestMapping("/updateUser")
     @ResponseBody
     public CommonResponseDto updateUser(@RequestBody @Validated UserDto userDto) {
-        CommonResponseDto dto = new CommonResponseDto();
-        return dto;
+        CommonResponseDto ret = new CommonResponseDto();
+        try {
+            userService.updateUser(userDto);
+            ret.setCode(myconst.SUCCESS);
+        } catch (Exception e) {
+            logger.error("UserController updateUser error");
+            ret.setCode(myconst.EXCEPTION);
+            e.printStackTrace();
+        }
+        return ret;
     }
 
-    @RequestMapping("/selectUser")
+    /**
+     * 查询用户通过用户id
+     *
+     * @param userDto
+     * @return
+     */
+    @RequestMapping("/selectUserById")
     @ResponseBody
-    public List<UserDto> selectUser(@RequestBody @Validated UserDto userDto) {
-        List<UserDto> uList = new ArrayList<>();
-        return uList;
+    public CommonResponseDto selectUserById(@RequestBody @Validated UserDto userDto) {
+        CommonResponseDto ret = new CommonResponseDto();
+        try {
+            ret.setData(userService.selectUserById(userDto));
+            ret.setCode(myconst.SUCCESS);
+        } catch (Exception e) {
+            logger.error("UserController selectUserById error");
+            ret.setCode(myconst.EXCEPTION);
+            e.printStackTrace();
+        }
+        return ret;
     }
+
+    /**
+     * 查询所有用户列表
+     *
+     * @param userDto
+     * @return
+     */
+    @RequestMapping("/selectAllUser")
+    @ResponseBody
+    public CommonResponseDto selectAllUser(@RequestBody @Validated UserDto userDto) {
+        CommonResponseDto ret = new CommonResponseDto();
+        List<UserDto> uList = new ArrayList<>();
+        //TODO 从Service层查询
+        ret.setData(uList);
+        return ret;
+    }
+
 }
