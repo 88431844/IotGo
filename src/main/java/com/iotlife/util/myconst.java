5f24f1d36f72219d8844cb243d1e635638242cb0
miracle <88431844@163.com>
2017-09-28 00:32:20 +0800

    常量类 分类 为 通用常量类、用户相关常量类、设备相关常量类
    Signed-off-by: miracle <88431844@163.com>

--- src/main/java/com/iotlife/util/myconst.java
+++ src/main/java/com/iotlife/util/myconst.java
@@ -1,10 +1,16 @@
 package com.iotlife.util;
 
 public class myconst {
-    public static final String SUCCESS = "1111";
-    public static final String FAIL = "9999";
-    public static final String EMPTY_LIST = "1000";
-    public static final String LOGIN_FAIL = "登录失败";
-    public static final String LOGIN_SUCCESS = "登录成功";
-    public static final String NULL_RESULT = "9990";
+    //////////通用常量 从1000 到 1999/////////////////
+    public static final String SUCCESS = "1000";
+    public static final String EXCEPTION = "1990";
+    public static final String FAIL = "1999";
+    public static final String EMPTY_LIST = "1001";
+    public static final String NULL_RESULT = "1002";
+    //////////用户相关常量 从2000 到 2999/////////////////
+
+
+    //////////设备相关常量 从3000 到 3999/////////////////
+    public static final String DEV_HAVE_Bind_CODE = "3001";
+
 }
