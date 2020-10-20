f118e96ff24779de23c131918b1b3e293571b83d
醉家男煮饺 <88431844@163.com>
2020-10-20 11:14:02 +0800

    去掉mapper注解

--- src/main/java/fun/iotgo/dao/TodoMapper.java
+++ src/main/java/fun/iotgo/dao/TodoMapper.java
@@ -2,11 +2,10 @@ package fun.iotgo.dao;
 
 import fun.iotgo.dto.TodoDto;
 import fun.iotgo.entity.Todo;
-import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-@Mapper
+
 public interface TodoMapper {
     int deleteByPrimaryKey(Integer id);
 
