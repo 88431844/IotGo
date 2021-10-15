2a4417b534e97ae1346d342982cd590ddc7e4ec6
醉家男煮饺 <88431844@163.com>
2021-10-15 11:22:35 +0800

    add TodoistController

--- src/main/java/fun/iotgo/controller/TodoistController.java
+++ src/main/java/fun/iotgo/controller/TodoistController.java
@@ -1,11 +1,8 @@
 package fun.iotgo.controller;
 
 import fun.iotgo.dto.CommonResponseDto;
-import fun.iotgo.dto.HttpDto;
 import fun.iotgo.util.myconst;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,22 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
  * https转换controller
  */
 @RestController
-@RequestMapping("/https")
+@RequestMapping("/todoist")
 @Slf4j
-public class HttpsRouterController {
-
+public class TodoistController {
 
     /**
-     * http请求转换为https
+     * 获取项目列表
      */
-    @RequestMapping("/http2https")
+    @RequestMapping("/getProjects")
     @ResponseBody
-    public CommonResponseDto http2https(@RequestBody @Validated HttpDto httpDto) {
+    public CommonResponseDto getProjects() {
         CommonResponseDto ret = new CommonResponseDto();
         try {
             System.out.println("");
         } catch (Exception e) {
-            log.error("HttpsRouterController http2https error");
+            log.error("TodoistController getProjects error");
             ret.setCode(myconst.EXCEPTION);
             e.printStackTrace();
         }
