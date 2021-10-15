7f92925b375cbabb477a7f12e69f0aa70f408ce0
醉家男煮饺 <88431844@163.com>
2021-10-15 11:45:26 +0800

    公共类增加响应时间

--- src/main/java/fun/iotgo/dto/CommonResponseDto.java
+++ src/main/java/fun/iotgo/dto/CommonResponseDto.java
@@ -2,6 +2,9 @@ package fun.iotgo.dto;
 
 import lombok.Data;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * 返回公共类
  */
@@ -10,5 +13,6 @@ public class CommonResponseDto<T> {
     private String code;
     private String msg;
     private T data;
+    private String responseTime = new SimpleDateFormat("YYYY-MM-dd HH:mm").format(new Date());
 
 }
